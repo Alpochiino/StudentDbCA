c53d7cac4fcc6772d619808973b0cc87a2e935dd
Christian Alp <Chrillealp11@hotmail.com>
2025-01-13 16:06:25 +0100

    Added create

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -2,7 +2,7 @@ namespace StudentWebApp.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using Models;
+using StudentWebApp.Models;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -18,11 +18,36 @@ public class StudentController : Controller
 
 	public async Task<IActionResult> Index()
 	{
-    	var response = await _httpClient.GetStringAsync("https://databaseca-f0bmfzchfccuasg2.northeurope-01.azurewebsites.net/api/students");
-        var students = JsonConvert.DeserializeObject<List<Student>>(response);
+		var response = await _httpClient.GetStringAsync("https://databaseca-f0bmfzchfccuasg2.northeurope-01.azurewebsites.net/api/students");
+		var students = JsonConvert.DeserializeObject<List<Student>>(response);
 
 		return View(students);
 	}
+	
+/* 	[HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    } */
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Student student)
+    {
+        if (ModelState.IsValid)
+        {
+            var json = JsonConvert.SerializeObject(student);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("api/students", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        return View(student);
+    }
 
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 	public IActionResult Error()
