fae6ef26c1a1f6f17de97edd7abf24c12081f897
Christian Alp <Chrillealp11@hotmail.com>
2025-01-19 21:22:19 +0100

    Updated spacing

--- Models/Student.cs
+++ Models/Student.cs
@@ -3,7 +3,10 @@ namespace StudentWebApp.Models;
 public class Student
 {
 	public int Id { get; set; }
+
 	public string? Name { get; set; }
+
 	public int Age { get; set; }
+
 	public string? Major { get; set; }
 }
